2954b4cd0b915c8403e51d1dd0c656f731382450
saawanth <saawanth.j@gmail.com>
2018-03-25 19:42:02 -0400

    changes for sairam branch

--- Datastructure/src/oops2/Rectangle.java
+++ Datastructure/src/oops2/Rectangle.java
@@ -10,6 +10,6 @@ public class Rectangle implements Shape {
 	public void draw() {
 		System.out.println("Rectangle Drawn");
 		System.out.println("Testing this application xyz");
-		System.out.println("Git Testing");
+		System.out.println("Git Testing for sai ram");
 	}
 }
