ae3ecac30226b7419d85439541ce3040244d5f08
saawanth <saawanth.j@gmail.com>
2018-03-25 20:25:32 -0400

    backto normal changes

--- Datastructure/src/oops2/Rectangle.java
+++ Datastructure/src/oops2/Rectangle.java
@@ -9,7 +9,5 @@ public class Rectangle implements Shape {
      }
 	public void draw() {
 		System.out.println("Rectangle Drawn");
-		System.out.println("Testing this application xyz");
-		System.out.println("Git Testing for sai ram branch to confilt");
 	}
 }
